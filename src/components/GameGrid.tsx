37e7ace9bd384e12e8bbe9040f055dad95493873
Melad01 <meladnofal145@gmail.com>
2023-05-09 18:21:29 -0700

    Displaying Critic Score

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ const GameGrid = () => {
           {error}
         </Text>
       )}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10} padding={5}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={15} padding={5}>
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
